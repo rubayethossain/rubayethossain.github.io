ecb87fb1ae54b9f3af48b1e625f631596764ca75
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-08-21 12:36:23 +0600

    Install and Setup Tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Md. Rubayet Hossain | Frontend Developer`,
+    description: `Portfolio & Blog of Md. Rubayet Hossain`,
+    author: `@rubayethossain`,
+    siteUrl: `https://rubayet.dev/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,5 +33,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    "gatsby-plugin-postcss",
   ],
 }
