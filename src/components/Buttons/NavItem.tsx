99d0566537c49e467407027aebb55dea0b075bc0
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-06 23:50:56 +0600

    Add icons to navitem button

--- src/components/Buttons/NavItem.tsx
+++ src/components/Buttons/NavItem.tsx
@@ -1,15 +1,19 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
 
 interface Props {
   children: React.ReactNode
   onClick?: () => void
   className?: string
   style?: React.CSSProperties
+  icon: IconProp
 }
 
-const NavItem = ({ children, onClick, className, style }: Props) => {
+const NavItem = ({ children, onClick, className, style, icon }: Props) => {
   return (
     <button onClick={onClick} className={className} style={style}>
+      <FontAwesomeIcon icon={icon} />
       {children}
     </button>
   )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,12 @@
 import { navigate } from "gatsby"
 import React from "react"
 import { NavItem } from "./Buttons"
+import {
+  faDiceD6,
+  faCode,
+  faAddressCard,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons"
 
 const Nav = () => {
   const changeRoute = (path: string): void => {
@@ -12,19 +18,31 @@ const Nav = () => {
     <aside className="w-32">
       <ul>
         <li>
-          <NavItem onClick={changeRoute.bind(null, "/")}>Home</NavItem>
+          <NavItem onClick={changeRoute.bind(null, "/")} icon={faDiceD6}>
+            Home
+          </NavItem>
         </li>
 
         <li>
-          <NavItem onClick={changeRoute.bind(null, "/works")}>Works</NavItem>
+          <NavItem onClick={changeRoute.bind(null, "/works")} icon={faCode}>
+            Works
+          </NavItem>
         </li>
 
         <li>
-          <NavItem onClick={changeRoute.bind(null, "/resume")}>Resume</NavItem>
+          <NavItem
+            onClick={changeRoute.bind(null, "/resume")}
+            icon={faAddressCard}
+          >
+            Resume
+          </NavItem>
         </li>
 
         <li>
-          <NavItem onClick={changeRoute.bind(null, "/contact")}>
+          <NavItem
+            onClick={changeRoute.bind(null, "/contact")}
+            icon={faEnvelope}
+          >
             Contact
           </NavItem>
         </li>
