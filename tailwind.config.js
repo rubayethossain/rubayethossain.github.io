d3361746cc2ebd61d9dc6618e7ce54526777bf5d
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-19 20:20:34 +0600

    Change font family to Quicksand

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['"Nunito"', "sans-serif"],
+      sans: ['"Quicksand"', "sans-serif"],
     },
     extend: {},
   },
