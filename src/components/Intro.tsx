2fa64a49a5600e761c4b137a1956eade59caf63a
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-26 23:21:55 +0600

    Replace slogan with social icons list

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,67 +4,63 @@ import { StaticImage } from "gatsby-plugin-image"
 const Intro = (): JSX.Element => {
   return (
     <header className="mx-auto max-w-screen-lg shadow-md bg-white flex overflow-hidden my-10">
-      <div className="flex">
-        <StaticImage
-          src="../images/rubayet.jpg"
-          width={300}
-          quality={85}
-          alt="Md. Ruabyet Hossain"
-          className=""
-        />
-        <div className="p-8 space-y-4">
-          <h2 className="text-3xl font-bold uppercase text-gray-700">
-            Md. Rubayet Hossain
-          </h2>
-          <h2 className="text-2xl font-semibold text-gray-600">
-            Professional Frontend Developer
-          </h2>
-          <p className="font-semibold text-base text-gray-600">
-            Passionate Frontend Developer with 5+ years of professional
-            experience! Love React, JavaScript, HTML & CSS.
-          </p>
-        </div>
+      <StaticImage
+        src="../images/rubayet.jpg"
+        width={215}
+        quality={85}
+        alt="Md. Ruabyet Hossain"
+      />
+      <div className="p-8 space-y-4 flex-1">
+        <h2 className="text-3xl font-bold uppercase text-gray-700">
+          Md. Rubayet Hossain
+        </h2>
+        <h2 className="text-2xl font-semibold text-gray-600">
+          Professional Frontend Developer
+        </h2>
+
+        <ul>
+          <li className="inline-block mr-2">
+            <a href="#">
+              <i className="flaticon-linkedin"></i>
+            </a>
+          </li>
+          <li className="inline-block mx-2">
+            <a href="#">
+              <i className="flaticon-github"></i>
+            </a>
+          </li>
+          <li className="inline-block mx-2">
+            <a href="#">
+              <i className="flaticon-stack-overflow"></i>
+            </a>
+          </li>
+          <li className="inline-block mx-2">
+            <a href="#">
+              <i className="flaticon-medium"></i>
+            </a>
+          </li>
+          <li className="inline-block ml-2">
+            <a href="#">
+              <i className="flaticon-twitter"></i>
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <div className="p-8 w-80">
-        <div className="mb-4">
+      <div className="p-8 space-y-4 w-64">
+        <div>
+          <h3 className="font-bold">Currently Working</h3>
+          <p className="font-semibold text-gray-600">CEFALO Bangladesh</p>
+        </div>
+
+        <div>
           <h3 className="font-bold">Location</h3>
           <p className="font-semibold text-gray-600">Dhaka, Bangladesh</p>
         </div>
 
-        <div className="mb-4">
+        <div>
           <h3 className="font-bold">Email</h3>
           <p className="font-semibold text-gray-600">hi@rubayet.dev</p>
-        </div>
-
-        <div>
-          <ul>
-            <li className="inline-block mr-2">
-              <a href="#">
-                <i className="flaticon-linkedin"></i>
-              </a>
-            </li>
-            <li className="inline-block mx-2">
-              <a href="#">
-                <i className="flaticon-github"></i>
-              </a>
-            </li>
-            <li className="inline-block mx-2">
-              <a href="#">
-                <i className="flaticon-stack-overflow"></i>
-              </a>
-            </li>
-            <li className="inline-block mx-2">
-              <a href="#">
-                <i className="flaticon-medium"></i>
-              </a>
-            </li>
-            <li className="inline-block ml-2">
-              <a href="#">
-                <i className="flaticon-twitter"></i>
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
     </header>
