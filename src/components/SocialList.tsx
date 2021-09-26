14deedb3ab76968ada760893777c150024d9a1c3
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-26 23:34:08 +0600

    Change color on social icon hover

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -4,27 +4,47 @@ const SocialList = (): JSX.Element => {
   return (
     <ul>
       <li className="inline-block mr-2">
-        <a href="#">
+        <a
+          href="#"
+          className="hover:text-linkedin transition-colors"
+          title="Linkedin"
+        >
           <i className="flaticon-linkedin text-2xl"></i>
         </a>
       </li>
       <li className="inline-block mx-2">
-        <a href="#">
+        <a
+          href="#"
+          className="hover:text-github transition-colors"
+          title="GitHub"
+        >
           <i className="flaticon-github text-2xl"></i>
         </a>
       </li>
       <li className="inline-block mx-2">
-        <a href="#">
+        <a
+          href="#"
+          className="hover:text-stackOverflow transition-colors"
+          title="StackOverflow"
+        >
           <i className="flaticon-stack-overflow text-2xl"></i>
         </a>
       </li>
       <li className="inline-block mx-2">
-        <a href="#">
+        <a
+          href="#"
+          className="hover:text-medium transition-colors"
+          title="Medium"
+        >
           <i className="flaticon-medium text-2xl"></i>
         </a>
       </li>
       <li className="inline-block ml-2">
-        <a href="#">
+        <a
+          href="#"
+          className="hover:text-twitter transition-colors"
+          title="Twitter"
+        >
           <i className="flaticon-twitter text-2xl"></i>
         </a>
       </li>
