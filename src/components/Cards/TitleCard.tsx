4f461bbaee74aa88abffab25ceaaf49948dafaa2
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-26 23:36:36 +0600

    Set icon size

--- src/components/Cards/TitleCard.tsx
+++ src/components/Cards/TitleCard.tsx
@@ -9,7 +9,7 @@ const TitleCard = (props: Props) => {
   return (
     <div className="border border-gray-900 rounded h-20 flex items-center">
       <div className="w-20 h-20 flex justify-center items-center border-r border-gray-900">
-        {props.icon}
+        <span className="text-5xl">{props.icon}</span>
       </div>
       <h3 className="p-3 text-lg capitalize font-bold text-gray-600">
         {props.title}
