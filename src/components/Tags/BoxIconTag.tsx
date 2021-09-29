864ea452d9613f32254497ee475016817f045897
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-09-29 23:40:54 +0600

    Design box icon tag component

--- src/components/Tags/BoxIconTag.tsx
+++ src/components/Tags/BoxIconTag.tsx
@@ -7,9 +7,14 @@ interface Props {
 
 const BoxIconTag = ({ icon, title }: Props) => {
   return (
-    <div>
-      <img src={icon} alt={title} />
-      <p>{title}</p>
+    <div className="w-24 h-24 border-2 border-gray-700 rounded overflow-hidden flex flex-col">
+      <div className="flex-1 flex flex-col justify-center">
+        <img src={icon} alt={title} className="h-12 block mx-auto max-w-full" />
+      </div>
+
+      <p className="text-center border-gray-700 font-semibold bg-gray-700 text-white">
+        {title}
+      </p>
     </div>
   )
 }
