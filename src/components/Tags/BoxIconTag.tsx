e0608f8a0219ce31141e2ba08014e0e613d3fabe
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-10-09 11:40:19 +0600

    Add class and style prop

--- src/components/Tags/BoxIconTag.tsx
+++ src/components/Tags/BoxIconTag.tsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 
 interface Props {
   icon: string
   title: string
+  className?: string
+  style?: CSSProperties
 }
 
-const BoxIconTag = ({ icon, title }: Props) => {
+const BoxIconTag = ({ icon, title, className, style }: Props) => {
   return (
-    <div className="w-24 h-24 border-2 border-gray-700 rounded overflow-hidden flex flex-col">
+    <div
+      className={`w-24 h-24 border-2 border-gray-700 rounded overflow-hidden flex flex-col ${
+        className || ""
+      }`}
+      style={style}
+    >
       <div className="flex-1 flex flex-col justify-center">
         <img src={icon} alt={title} className="h-12 block mx-auto max-w-full" />
       </div>
