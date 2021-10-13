ca99f06cd4b90076c55e7e37ba4cbac78fddfe6d
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-10-13 23:19:54 +0600

    feat: add hover design change

--- src/components/Buttons/NavItem.tsx
+++ src/components/Buttons/NavItem.tsx
@@ -11,7 +11,7 @@ const NavItem = ({ title, onClick, className, style, icon }: Props) => {
   return (
     <button
       onClick={onClick}
-      className={`flex flex-col items-center justify-center w-28 h-24 border bg-white ${className}`}
+      className={`flex flex-col items-center justify-center w-28 h-24 bg-white hover:bg-gray-600 hover:text-white transition-colors duration-300 ${className}`}
       style={style}
     >
       <span className=" text-2xl">{icon}</span>
