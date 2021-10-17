03df40feb3693293c02ece251d6516daf15ae2f3
Md. Rubayet Hossain <rubayet33@gmail.com>
2021-10-17 12:24:38 +0600

    Use TextTag component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import SocialList from "./SocialList"
+import { TextTag } from "./Tags"
 
 const Intro = (): JSX.Element => {
   return (
@@ -26,18 +27,24 @@ const Intro = (): JSX.Element => {
 
       <div className="p-5 sm:p-8 space-y-3 sm:space-y-4 sm:w-64">
         <div className="text-center sm:text-left">
-          <h3 className="font-bold">Working @</h3>
-          <p className="font-semibold text-gray-600">CEFALO Bangladesh</p>
+          <h3 className="font-bold mb-1">Working @</h3>
+          <p>
+            <TextTag>CEFALO</TextTag>
+          </p>
         </div>
 
         <div className="text-center sm:text-left">
-          <h3 className="font-bold">Location</h3>
-          <p className="font-semibold text-gray-600">Dhaka, Bangladesh</p>
+          <h3 className="font-bold mb-1">Location</h3>
+          <p>
+            <TextTag>Dhaka, Bangladesh</TextTag>
+          </p>
         </div>
 
         <div className="text-center sm:text-left">
-          <h3 className="font-bold">Email</h3>
-          <p className="font-semibold text-gray-600">hi@rubayet.dev</p>
+          <h3 className="font-bold mb-1">Email</h3>
+          <p>
+            <TextTag>hi@rubayet.dev</TextTag>
+          </p>
         </div>
       </div>
     </header>
